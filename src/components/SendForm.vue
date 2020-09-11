b112470bf5359a59014af6fcdd6dff6760fcaa7e
fanjiangfeng <fantongxue666@163.com>
2020-09-11 18:07:51 +0800

    axios跨域配置，发表朋友圈，上传图片

--- src/components/SendForm.vue
+++ src/components/SendForm.vue
@@ -1,16 +1,18 @@
 <template>
   <div style="width:60%;margin:auto;padding-top:50px;">
-    <el-form ref="form" :model="form" label-width="80px">
+    <el-form label-width="80px">
       <el-form-item label="发表内容">
-        <el-input type="textarea" v-model="form.desc"></el-input>
+        <el-input type="textarea" v-model="content"></el-input>
       </el-form-item>
 
       <el-upload
         class="upload-demo"
-        action="https://jsonplaceholder.typicode.com/posts/"
+        action="/api/lifespace/uploadImage"
+        name="file"
         :on-preview="handlePreview"
         :on-remove="handleRemove"
         :file-list="fileList"
+        :on-success="uploadSuccess"
         list-type="picture"
       >
         <el-button size="small" type="primary">点击上传</el-button>
@@ -19,8 +21,8 @@
 
       <div style="height:30px;"></div>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">发表老子的见解</el-button>
-        <el-button>取消</el-button>
+        <el-button type="primary" @click="onSubmit">走你</el-button>
+        <el-button @click="back">还是算求吧</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -31,38 +33,57 @@ export default {
   name: "SendForm",
   data() {
     return {
-      fileList: [
-        {
-          name: "food.jpeg",
-          url:
-            "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100"
-        },
-        {
-          name: "food2.jpeg",
-          url:
-            "https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100"
-        }
-      ],
-      form: {
-        name: "",
-        region: "",
-        date1: "",
-        date2: "",
-        delivery: false,
-        type: [],
-        resource: "",
-        desc: ""
-      }
+      content: "",
+      fileList: [],
+      picList: []
     };
   },
   methods: {
+    uploadSuccess(response, file, fileList) {
+      const array = response;
+      console.log(array);
+      this.picList.push(array);
+    },
+    back() {
+      this.$router.go(-1);
+    },
     onSubmit() {
-      console.log("submit!");
+      const contents = this.content;
+      if (this.content == "" || this.content == undefined) {
+        this.$message("完善发表内容，大兄弟！");
+        return;
+      } else {
+        console.log(this.fileList);
+
+        // axios请求开始
+        this.axios
+          .post("/api/lifespace/saveContent", {
+            content: this.content,
+            picList: this.picList
+          })
+          .then(response => {
+            console.log(response.data);
+            if (response.data == 1) {
+              this.$message({
+                message: "发送成功",
+                type: "success"
+              });
+              this.$router.go(-1);
+            }
+          })
+          .catch(function(error) {
+            this.$message.error('服务器发生故障');
+
+          });
+        // axios请求结束
+      }
     },
     handleRemove(file, fileList) {
+      console.log("删除图片触发");
       console.log(file, fileList);
     },
     handlePreview(file) {
+      console.log("222222222");
       console.log(file);
     }
   }
